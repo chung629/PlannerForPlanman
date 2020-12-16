7f231194520160a4890032f1e7ea8314c10ac51d
chunghyun <chung629@naver.com>
2020-12-16 20:20:02 +0900

    calendar temp complete

--- app/src/main/java/org/chunghyun/plannerforplanman/Fragment_calendar.java
+++ app/src/main/java/org/chunghyun/plannerforplanman/Fragment_calendar.java
@@ -3,40 +3,41 @@ package org.chunghyun.plannerforplanman;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import android.content.Context;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_calendar#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment_calendar extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    
+    // 캘린더 관련
+    private TextView tvDate;
+    private GridAdapter gridAdapter;
+    private ArrayList<String> dayList;
+    private GridView gridView;
+    private Calendar mCal;
 
     public Fragment_calendar() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_calendar.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment_calendar newInstance(String param1, String param2) {
         Fragment_calendar fragment = new Fragment_calendar();
         Bundle args = new Bundle();
@@ -54,11 +55,135 @@ public class Fragment_calendar extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        //초기화
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        tvDate = (TextView) view.findViewById(R.id.tv_date);
+        gridView = (GridView)view.findViewById(R.id.gridview);
+        Button leftButton = view.findViewById(R.id.leftCalendar);
+        Button rightButton = view.findViewById(R.id.rightCalendar);
+
+        // 오늘에 날짜를 세팅 해준다.
+        long now = System.currentTimeMillis();
+        final Date date = new Date(now);
+        //연,월,일을 따로 저장
+        final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
+        final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
+        final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
+
+        //현재 년/월 캘린더 title
+        tvDate.setText(curYearFormat.format(date) + "년   " + curMonthFormat.format(date) + "월");
+
+        dayList = new ArrayList<String>();
+        mCal = Calendar.getInstance();
+
+        //이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
+        mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
+        int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
+        //1일 - 요일 매칭 시키기 위해 공백 add
+        for (int i = 1; i < dayNum; i++) {
+            dayList.add("");
+        }
+        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
+
+        gridAdapter = new GridAdapter(view.getContext(), dayList);
+        gridView.setAdapter(gridAdapter);
+
+        // 버튼 관련
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+
+        return view;
     }
+
+    //캘린더 관련
+    /**
+     * 해당 월에 표시할 일 수 구함
+     *
+     * @param month
+     */
+    private void setCalendarDate(int month) {
+        mCal.set(Calendar.MONTH, month - 1);
+
+        for (int i = 0; i < mCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+            dayList.add("" + (i + 1));
+        }
+
+    }
+
+    /**
+     * 그리드뷰 어댑터
+     *
+     */
+    private class GridAdapter extends BaseAdapter {
+
+        private final List<String> list;
+        private final LayoutInflater inflater;
+
+        /**
+         * 생성자
+         *
+         * @param context
+         * @param list
+         */
+        public GridAdapter(Context context, List<String> list) {
+            this.list = list;
+            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_calendar_gridview, parent, false);
+                holder = new ViewHolder();
+
+                holder.tvItemGridView = (TextView)convertView.findViewById(R.id.tv_item_gridview);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+            holder.tvItemGridView.setText("" + getItem(position));
+
+            //해당 날짜 텍스트 컬러,배경 변경
+            mCal = Calendar.getInstance();
+            //오늘 day 가져옴
+            Integer today = mCal.get(Calendar.DAY_OF_MONTH);
+            String sToday = String.valueOf(today);
+            if (sToday.equals(getItem(position))) { //오늘 day 텍스트 컬러 변경
+                holder.tvItemGridView.setTextColor(getResources().getColor(R.color.black));
+            }
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        TextView tvItemGridView;
+    }
+
 }
