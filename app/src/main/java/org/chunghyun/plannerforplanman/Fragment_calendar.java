2bead8178015ab0e59279e15ad907afadb140e98
chunghyun <chung629@naver.com>
2020-12-16 20:58:22 +0900

    calendar complete

--- app/src/main/java/org/chunghyun/plannerforplanman/Fragment_calendar.java
+++ app/src/main/java/org/chunghyun/plannerforplanman/Fragment_calendar.java
@@ -18,35 +18,23 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 public class Fragment_calendar extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     private String mParam1;
     private String mParam2;
 
-    
-    // 캘린더 관련
-    private TextView tvDate;
-    private GridAdapter gridAdapter;
-    private ArrayList<String> dayList;
-    private GridView gridView;
-    private Calendar mCal;
+    //캘린더 관련
+    MaterialCalendarView materialCalendarView;
 
     public Fragment_calendar() {
         // Required empty public constructor
     }
-
-    public static Fragment_calendar newInstance(String param1, String param2) {
-        Fragment_calendar fragment = new Fragment_calendar();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,119 +46,23 @@ public class Fragment_calendar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //초기화
+
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-        tvDate = (TextView) view.findViewById(R.id.tv_date);
-        gridView = (GridView)view.findViewById(R.id.gridview);
+        materialCalendarView = view.findViewById(R.id.calendarView);
 
-        // 오늘에 날짜를 세팅 해준다.
-        long now = System.currentTimeMillis();
-        final Date date = new Date(now);
-        //연,월,일을 따로 저장
-        final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
-        final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
-        final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
+        // 달력의 시작과 끝 지정
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(1990, 0, 1))
+                .setMaximumDate(CalendarDay.from(2050, 12, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
 
-        //현재 년/월 캘린더 title
-        tvDate.setText(curYearFormat.format(date) + "년   " + curMonthFormat.format(date) + "월");
-
-        dayList = new ArrayList<String>();
-        mCal = Calendar.getInstance();
-
-        //이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
-        mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
-        int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
-        //1일 - 요일 매칭 시키기 위해 공백 add
-        for (int i = 1; i < dayNum; i++) {
-            dayList.add("");
-        }
-        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
-
-        gridAdapter = new GridAdapter(view.getContext(), dayList);
-        gridView.setAdapter(gridAdapter);
-
+        // 달력 효과 지정
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                new OneDayDecorator());
         return view;
-    }
-
-    //캘린더 관련
-    /**
-     * 해당 월에 표시할 일 수 구함
-     *
-     * @param month
-     */
-    private void setCalendarDate(int month) {
-        mCal.set(Calendar.MONTH, month - 1);
-
-        for (int i = 0; i < mCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
-            dayList.add("" + (i + 1));
-        }
-
-    }
-
-    /**
-     * 그리드뷰 어댑터
-     *
-     */
-    private class GridAdapter extends BaseAdapter {
-
-        private final List<String> list;
-        private final LayoutInflater inflater;
-
-        /**
-         * 생성자
-         *
-         * @param context
-         * @param list
-         */
-        public GridAdapter(Context context, List<String> list) {
-            this.list = list;
-            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-
-        @Override
-        public int getCount() {
-            return list.size();
-        }
-
-        @Override
-        public String getItem(int position) {
-            return list.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            ViewHolder holder = null;
-
-            if (convertView == null) {
-                convertView = inflater.inflate(R.layout.item_calendar_gridview, parent, false);
-                holder = new ViewHolder();
-
-                holder.tvItemGridView = (TextView)convertView.findViewById(R.id.tv_item_gridview);
-
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder)convertView.getTag();
-            }
-            holder.tvItemGridView.setText("" + getItem(position));
-
-            //해당 날짜 텍스트 컬러,배경 변경
-            mCal = Calendar.getInstance();
-            //오늘 day 가져옴
-            Integer today = mCal.get(Calendar.DAY_OF_MONTH);
-            String sToday = String.valueOf(today);
-            if (sToday.equals(getItem(position))) { //오늘 day 텍스트 컬러 변경
-                holder.tvItemGridView.setTextColor(getResources().getColor(R.color.black));
-            }
-            return convertView;
-        }
-    }
-    private class ViewHolder {
-        TextView tvItemGridView;
     }
 }
