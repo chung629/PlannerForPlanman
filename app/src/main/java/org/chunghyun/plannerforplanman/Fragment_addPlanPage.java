70f61e51ac5f2100148f2243b134a5a2d84b3a4f
chunghyun <chung629@naver.com>
2020-12-21 21:11:19 +0900

    home_dialog complete

--- app/src/main/java/org/chunghyun/plannerforplanman/Fragment_addPlanPage.java
+++ app/src/main/java/org/chunghyun/plannerforplanman/Fragment_addPlanPage.java
@@ -10,40 +10,16 @@ import android.view.ViewGroup;
 
 public class Fragment_addPlanPage extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment_addPlanPage() {
-        // Required empty public constructor
-    }
-
-    public static Fragment_addPlanPage newInstance(String param1, String param2) {
-        Fragment_addPlanPage fragment = new Fragment_addPlanPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_add_plan_page, container, false);
-
         initUI(rootView);
-
         return rootView;
     }
     private void initUI(ViewGroup rootView){
--- app/src/main/java/org/chunghyun/plannerforplanman/Fragment_calendar.java
+++ app/src/main/java/org/chunghyun/plannerforplanman/Fragment_calendar.java
@@ -15,24 +15,12 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class Fragment_calendar extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
-
     //캘린더 관련
     MaterialCalendarView materialCalendarView;
 
-    public Fragment_calendar() {
-        // Required empty public constructor
-    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
